4101d8a5d9a1125a9e906f7850724e896bfea15e
Hieugamingmc747 <alexanderaventarofisher@gmail.com>
2025-03-23 20:20:58 +0700

    Update BypassYeumoneyUpdate.user.js

--- BypassYeumoneyUpdate.user.js
+++ BypassYeumoneyUpdate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yeumoney Bypasser By HieuDzPro
 // @namespace    http://tampermonkey.net/
-// @version      11.0
+// @version      11.1
 // @description  The Yeumoney Bypasser Chính thức (Hieudz * Ducknovis * XGreen)
 // @author       HieuDz Code
 // @match        *://yeumoney.com/*
@@ -9,6 +9,8 @@
 // @match        *://docs.google.com/forms/*
 // @grant        GM_xmlhttpRequest
 // @require      https://raw.githubusercontent.com/Hieugamingmc747/BypassYeumoney/refs/heads/main/BypassYeumoney.js
+// @updateURL    https://raw.githubusercontent.com/hieugamingmc747/BypassYeumoney/main/BypassYeumoneyUpdate.user.js
+// @downloadURL  https://raw.githubusercontent.com/hieugamingmc747/BypassYeumoney/main/BypassYeumoney.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @icon         https://tr.rbxcdn.com/180DAY-433e973b5fbc1e8d5cdebd0c6be03472/420/420/Decal/Webp/noFilter
